7a6f3ba8738dcebb2c13d4cb8920e1d8bef8372e
Rhymart Manchus <rhymart.manchus@gmail.com>
2020-08-03 23:02:36 +0800

    refactor activity
    Signed-off-by: Rhymart Manchus <rhymart.manchus@gmail.com>

--- app/src/main/java/com/rhymartmanchus/yelpassignment/ui/MainActivity.kt
+++ app/src/main/java/com/rhymartmanchus/yelpassignment/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.rhymartmanchus.yelpassignment
+package com.rhymartmanchus.yelpassignment.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rhymartmanchus.yelpassignment.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
