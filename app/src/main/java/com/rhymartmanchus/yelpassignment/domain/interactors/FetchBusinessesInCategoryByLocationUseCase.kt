94eaaf8c8453831534a2c280aecafa406fd4f73d
Rhymart Manchus <rhymart.manchus@gmail.com>
2020-08-07 05:02:27 +0800

    Added a use case to fetch businesses in a category by location. Closes #6
    Signed-off-by: Rhymart Manchus <rhymart.manchus@gmail.com>

--- app/src/main/java/com/rhymartmanchus/yelpassignment/domain/interactors/FetchBusinessesInCategoryByLocationUseCase.kt
+++ app/src/main/java/com/rhymartmanchus/yelpassignment/domain/interactors/FetchBusinessesInCategoryByLocationUseCase.kt
@@ -1,4 +1,53 @@
 package com.rhymartmanchus.yelpassignment.domain.interactors
 
-class FetchBusinessesInCategoryByLocationUseCase {
+import com.rhymartmanchus.yelpassignment.coroutines.AppCoroutineDispatcher
+import com.rhymartmanchus.yelpassignment.domain.BusinessesGateway
+import com.rhymartmanchus.yelpassignment.domain.SortingStrategy
+import com.rhymartmanchus.yelpassignment.domain.models.Business
+import com.rhymartmanchus.yelpassignment.domain.models.Category
+
+class FetchBusinessesInCategoryByLocationUseCase (
+    appCoroutinesDispatcher: AppCoroutineDispatcher,
+    private val gateway: BusinessesGateway
+) : UseCase<FetchBusinessesInCategoryByLocationUseCase.Response,
+        FetchBusinessesInCategoryByLocationUseCase.Params>(appCoroutinesDispatcher) {
+
+    override suspend fun run(params: Params): Response =
+        Response(
+            gateway.searchBusinesses(generateMapParams(params))
+        )
+
+    private fun generateMapParams(params: Params): Map<String, String> =
+        when(params.sortingStrategy) {
+            SortingStrategy.Default -> mapOf(
+                Pair("categories", params.category.alias),
+                Pair("limit", "${params.limit}"),
+                Pair("offset", "${params.offset}"),
+                Pair("latitude", "${params.latitude}"),
+                Pair("longitude", "${params.longitude}")
+            )
+            SortingStrategy.Rating,
+            SortingStrategy.Distance -> mapOf(
+                Pair("categories", params.category.alias),
+                Pair("limit", "${params.limit}"),
+                Pair("offset", "${params.offset}"),
+                Pair("latitude", "${params.latitude}"),
+                Pair("longitude", "${params.longitude}"),
+                Pair("sort_by", params.sortingStrategy.key)
+            )
+        }
+
+    data class Params (
+        val category: Category,
+        val limit: Int,
+        val offset: Int,
+        val latitude: Double,
+        val longitude: Double,
+        val sortingStrategy: SortingStrategy
+    )
+
+    data class Response (
+        val businesses: List<Business>
+    )
+
 }
--- app/src/test/java/com/rhymartmanchus/yelpassignment/domain/interactors/FetchBusinessesInCategoryByLocationUseCaseTest.kt
+++ app/src/test/java/com/rhymartmanchus/yelpassignment/domain/interactors/FetchBusinessesInCategoryByLocationUseCaseTest.kt
@@ -1,52 +1,241 @@
 package com.rhymartmanchus.yelpassignment.domain.interactors
 
+import com.rhymartmanchus.yelpassignment.argumentCaptor
+import com.rhymartmanchus.yelpassignment.capture
+import com.rhymartmanchus.yelpassignment.coroutines.TestAppCoroutinesDispatcher
+import com.rhymartmanchus.yelpassignment.domain.BusinessesGateway
 import com.rhymartmanchus.yelpassignment.domain.SortingStrategy
 import com.rhymartmanchus.yelpassignment.domain.exceptions.HttpRequestException
 import com.rhymartmanchus.yelpassignment.domain.exceptions.NoDataException
+import com.rhymartmanchus.yelpassignment.domain.models.Business
+import com.rhymartmanchus.yelpassignment.domain.models.Category
+import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertTrue
+import junit.framework.TestCase.assertFalse
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
 
-import org.junit.Assert.*
 import org.junit.Test
 import org.mockito.ArgumentMatchers
-import org.mockito.Mockito
+import org.mockito.Mock
+import org.mockito.Mockito.*
+import org.mockito.MockitoAnnotations
 import java.net.SocketTimeoutException
 
 class FetchBusinessesInCategoryByLocationUseCaseTest {
 
+    @Mock
+    private lateinit var gateway: BusinessesGateway
+
+    private lateinit var category: Category
+    private lateinit var useCase: FetchBusinessesInCategoryByLocationUseCase
+
     @Before
     fun setUp() {
+
+        MockitoAnnotations.initMocks(this)
+
+        category = Category(
+            "my-category",
+            "My Category"
+        )
+
+        useCase = FetchBusinessesInCategoryByLocationUseCase(
+            TestAppCoroutinesDispatcher(),
+            gateway
+        )
+
+    }
+
+    private suspend fun mockIdealRequest() {
+        `when`(gateway.searchBusinesses(ArgumentMatchers.anyMap()))
+            .then {
+                listOf(
+                    mock(Business::class.java),
+                    mock(Business::class.java)
+                )
+            }
     }
 
     @Test
     fun `should fetch businesses on a category with a given coordinates`() = runBlocking {
+
+        mockIdealRequest()
+
+        val result = useCase.execute(
+            FetchBusinessesInCategoryByLocationUseCase.Params(
+                category,
+                1,
+                1,
+                13.123,
+                123.13,
+                SortingStrategy.Default
+            )
+        )
+
+        assertEquals(2, result.businesses.size)
     }
 
     @Test(expected = NoDataException::class)
     fun `should throw an exception when no businesses found on a category in a given coordinates`() = runBlocking {
+        `when`(gateway.searchBusinesses(ArgumentMatchers.anyMap()))
+            .then {
+                throw NoDataException("No businesses available")
+            }
+
+        useCase.execute(
+            FetchBusinessesInCategoryByLocationUseCase.Params(
+                category,
+                1,
+                1,
+                13.123,
+                123.13,
+                SortingStrategy.Default
+            )
+        )
+
+        Unit
     }
 
     @Test(expected = SocketTimeoutException::class)
     fun `should throw an exception when nothing is responded`() = runBlocking {
+        `when`(gateway.searchBusinesses(ArgumentMatchers.anyMap()))
+            .then {
+                throw SocketTimeoutException()
+            }
+
+        useCase.execute(
+            FetchBusinessesInCategoryByLocationUseCase.Params(
+                category,
+                1,
+                1,
+                13.123,
+                123.13,
+                SortingStrategy.Default
+            )
+        )
+
+        Unit
     }
 
     @Test
     fun `params for gateway method should include location coordinates, category and limit-offset`() = runBlocking {
+        mockIdealRequest()
+
+        useCase.execute(
+            FetchBusinessesInCategoryByLocationUseCase.Params(
+                category,
+                50,
+                0,
+                13.123,
+                123.13,
+                SortingStrategy.Default
+            )
+        )
+
+        val captor = argumentCaptor<Map<String, String>>()
+
+        verify(gateway).searchBusinesses(capture(captor))
+
+        assertTrue(captor.value.containsKey("categories"))
+        assertTrue(captor.value.containsKey("latitude"))
+        assertTrue(captor.value.containsKey("longitude"))
+        assertTrue(captor.value.containsKey("limit"))
+        assertTrue(captor.value.containsKey("offset"))
+
+        assertEquals(category.alias, captor.value["categories"])
     }
 
     @Test(expected = HttpRequestException::class)
     fun `should throw an exception when max limit value for api has exceeded the maximum 50`() = runBlocking {
+        `when`(gateway.searchBusinesses(ArgumentMatchers.anyMap()))
+            .then {
+                throw HttpRequestException(
+                    "VALIDATION_ERROR",
+                    "error message",
+                    null
+                )
+            }
+
+        useCase.execute(
+            FetchBusinessesInCategoryByLocationUseCase.Params(
+                category,
+                50,
+                0,
+                13.123,
+                123.13,
+                SortingStrategy.Default
+            )
+        )
+
+        Unit
     }
 
     @Test
     fun `should include to params sort_by=rating when value for sortingstrategy is rating`() = runBlocking {
+        mockIdealRequest()
+
+        useCase.execute(
+            FetchBusinessesInCategoryByLocationUseCase.Params(
+                category,
+                50,
+                0,
+                13.123,
+                123.13,
+                SortingStrategy.Rating
+            )
+        )
+
+        val captor = argumentCaptor<Map<String, String>>()
+
+        verify(gateway).searchBusinesses(capture(captor))
+
+        assertTrue(captor.value.containsKey("sort_by"))
+        assertEquals("rating", captor.value["sort_by"])
     }
 
     @Test
     fun `should include to params sort_by=distance when value for sortingstrategy is distance`() = runBlocking {
+        mockIdealRequest()
+
+        useCase.execute(
+            FetchBusinessesInCategoryByLocationUseCase.Params(
+                category,
+                50,
+                0,
+                13.123,
+                123.13,
+                SortingStrategy.Distance
+            )
+        )
+
+        val captor = argumentCaptor<Map<String, String>>()
+
+        verify(gateway).searchBusinesses(capture(captor))
+
+        assertTrue(captor.value.containsKey("sort_by"))
+        assertEquals("distance", captor.value["sort_by"])
     }
 
     @Test
     fun `should not include sort_by params when sortingstrategy is default`() = runBlocking {
+        mockIdealRequest()
+
+        useCase.execute(
+            FetchBusinessesInCategoryByLocationUseCase.Params(
+                category,
+                50,
+                0,
+                13.123,
+                123.13,
+                SortingStrategy.Default
+            )
+        )
+
+        val captor = argumentCaptor<Map<String, String>>()
+
+        verify(gateway).searchBusinesses(capture(captor))
+
+        assertFalse(captor.value.containsKey("sort_by"))
     }
 }
