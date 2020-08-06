2213323a20477aa4041e676c6241c2f3b5dbff45
Rhymart Manchus <rhymart.manchus@gmail.com>
2020-08-06 21:41:19 +0800

    Added a use case to search a business using keyword. Closes #19
    Signed-off-by: Rhymart Manchus <rhymart.manchus@gmail.com>

--- app/src/main/java/com/rhymartmanchus/yelpassignment/domain/interactors/SearchBusinessesByKeywordUseCase.kt
+++ app/src/main/java/com/rhymartmanchus/yelpassignment/domain/interactors/SearchBusinessesByKeywordUseCase.kt
@@ -2,22 +2,58 @@ package com.rhymartmanchus.yelpassignment.domain.interactors
 
 import com.rhymartmanchus.yelpassignment.coroutines.AppCoroutineDispatcher
 import com.rhymartmanchus.yelpassignment.domain.BusinessesGateway
+import com.rhymartmanchus.yelpassignment.domain.SortingStrategy
+import com.rhymartmanchus.yelpassignment.domain.models.Business
 
 class SearchBusinessesByKeywordUseCase (
     appCoroutinesDispatcher: AppCoroutineDispatcher,
     private val gateway: BusinessesGateway
-) : UseCase<SearchBusinessesByKeywordUseCase.Response, SearchBusinessesByKeywordUseCase.Params>(appCoroutinesDispatcher) {
-
-    class Params (
-
-    )
-
-    class Response (
-
-    )
+) : UseCase<SearchBusinessesByKeywordUseCase.Response,
+        SearchBusinessesByKeywordUseCase.Params>(appCoroutinesDispatcher) {
 
     override suspend fun run(params: Params): Response {
-        TODO("Not yet implemented")
+
+        if(params.keyword.isBlank() || params.keyword.isEmpty())
+            throw IllegalArgumentException("`keyword` cannot be empty")
+
+        val result = gateway.searchBusinesses(
+            generateMapParams(params)
+        )
+
+        return Response(result)
     }
+
+    private fun generateMapParams(params: Params): Map<String, String> =
+        when(params.sortingStrategy) {
+            SortingStrategy.Default -> mapOf(
+                Pair("limit", "${params.limit}"),
+                Pair("offset", "${params.offset}"),
+                Pair("latitude", "${params.latitude}"),
+                Pair("longitude", "${params.longitude}"),
+                Pair("term", params.keyword)
+            )
+            SortingStrategy.Rating,
+            SortingStrategy.Distance -> mapOf(
+                Pair("limit", "${params.limit}"),
+                Pair("offset", "${params.offset}"),
+                Pair("latitude", "${params.latitude}"),
+                Pair("longitude", "${params.longitude}"),
+                Pair("sort_by", params.sortingStrategy.key),
+                Pair("term", params.keyword)
+            )
+        }
+
+    data class Params (
+        val keyword: String,
+        val limit: Int,
+        val offset: Int,
+        val latitude: Double,
+        val longitude: Double,
+        val sortingStrategy: SortingStrategy
+    )
+
+    data class Response (
+        val businesses: List<Business>
+    )
 
 }
--- app/src/test/java/com/rhymartmanchus/yelpassignment/domain/interactors/SearchBusinessesByKeywordUseCaseTest.kt
+++ app/src/test/java/com/rhymartmanchus/yelpassignment/domain/interactors/SearchBusinessesByKeywordUseCaseTest.kt
@@ -1,12 +1,24 @@
 package com.rhymartmanchus.yelpassignment.domain.interactors
 
+import com.rhymartmanchus.yelpassignment.argumentCaptor
+import com.rhymartmanchus.yelpassignment.capture
 import com.rhymartmanchus.yelpassignment.coroutines.TestAppCoroutinesDispatcher
 import com.rhymartmanchus.yelpassignment.domain.BusinessesGateway
+import com.rhymartmanchus.yelpassignment.domain.SortingStrategy
+import com.rhymartmanchus.yelpassignment.domain.exceptions.HttpRequestException
+import com.rhymartmanchus.yelpassignment.domain.exceptions.NoDataException
+import com.rhymartmanchus.yelpassignment.domain.models.Business
+import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertTrue
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
+import org.mockito.ArgumentMatchers
 
 import org.mockito.Mock
+import org.mockito.Mockito.*
 import org.mockito.MockitoAnnotations
+import java.io.IOException
 
 class SearchBusinessesByKeywordUseCaseTest {
 
@@ -27,48 +39,189 @@ class SearchBusinessesByKeywordUseCaseTest {
 
     }
 
-    @Test
-    fun `should fetch businesses by keyword in a location successfully`() {
-        TODO("Not yet implemented")
+    private suspend fun mockIdealRequest() {
+        `when`(gateway.searchBusinesses(ArgumentMatchers.anyMap()))
+            .then {
+                listOf(
+                    mock(Business::class.java),
+                    mock(Business::class.java)
+                )
+            }
     }
 
     @Test
-    fun `should throw an exception when no results for which relates to the keyword`() {
-        TODO("Not yet implemented")
+    fun `should fetch businesses by keyword in a location successfully`() = runBlocking {
+        mockIdealRequest()
+
+        val result = useCase.execute(
+            SearchBusinessesByKeywordUseCase.Params(
+                "Sample",
+                1,
+                1,
+                12.99,
+                123.92,
+                SortingStrategy.Default
+            )
+        )
+
+        assertEquals(2, result.businesses.size)
     }
 
     @Test
-    fun `should include limit and offset to params for paging`() {
-        TODO("Not yet implemented")
+    fun `should include term, coordinates, limit and offset in params when searching for a keyword`() = runBlocking {
+        mockIdealRequest()
+
+        useCase.execute(
+            SearchBusinessesByKeywordUseCase.Params(
+                "keyword",
+                1,
+                1,
+                13.999,
+                123.111,
+                SortingStrategy.Default
+            )
+        )
+
+        val captor = argumentCaptor<Map<String, String>>()
+
+        verify(gateway).searchBusinesses(capture(captor))
+
+        assertTrue(captor.value.containsKey("term"))
+        assertTrue(captor.value.containsKey("latitude"))
+        assertTrue(captor.value.containsKey("longitude"))
+        assertTrue(captor.value.containsKey("limit"))
+        assertTrue(captor.value.containsKey("offset"))
+        assertTrue(!captor.value.containsKey("sort_by"))
     }
 
     @Test
-    fun `should include term and coordinates in params when searching for a keyword`() {
-        TODO("Not yet implemented")
+    fun `should include sort_by=distance when trying to sort results by distance`() = runBlocking {
+        mockIdealRequest()
+
+        useCase.execute(
+            SearchBusinessesByKeywordUseCase.Params(
+                "keyword",
+                1,
+                1,
+                13.999,
+                123.111,
+                SortingStrategy.Distance
+            )
+        )
+
+        val captor = argumentCaptor<Map<String, String>>()
+
+        verify(gateway).searchBusinesses(capture(captor))
+
+        assertTrue(captor.value.containsKey("sort_by"))
+        assertEquals("distance", captor.value.get("sort_by"))
     }
 
     @Test
-    fun `should include sort_by=distance when trying to sort results by distance`() {
-        TODO("Not yet implemented")
+    fun `should include sort_by=rating when trying to sort results by rating`() = runBlocking {
+        mockIdealRequest()
+
+        useCase.execute(
+            SearchBusinessesByKeywordUseCase.Params(
+                "keyword",
+                1,
+                1,
+                13.999,
+                123.111,
+                SortingStrategy.Rating
+            )
+        )
+
+        val captor = argumentCaptor<Map<String, String>>()
+
+        verify(gateway).searchBusinesses(capture(captor))
+
+        assertTrue(captor.value.containsKey("sort_by"))
+        assertEquals("rating", captor.value.get("sort_by"))
     }
 
-    @Test
-    fun `should include sort_by=rating when trying to sort results by rating`() {
-        TODO("Not yet implemented")
+    @Test(expected = IOException::class)
+    fun `should throw an exception when ioexception happened`() = runBlocking {
+        `when`(gateway.searchBusinesses(ArgumentMatchers.anyMap()))
+            .then {
+                throw IOException()
+            }
+
+        useCase.execute(
+            SearchBusinessesByKeywordUseCase.Params(
+                "Sample",
+                1,
+                1,
+                12.12,
+                123.123,
+                SortingStrategy.Default
+            )
+        )
+
+        Unit
     }
 
-    @Test
-    fun `should throw an exception when the httprequest failed`() {
-        TODO("Not yet implemented")
+    @Test(expected = HttpRequestException::class)
+    fun `should throw an exception when limit param reached the maximum value 50`() = runBlocking {
+        `when`(gateway.searchBusinesses(ArgumentMatchers.anyMap()))
+            .then {
+                throw HttpRequestException(
+                    "VALIDATION_ERROR",
+                    "Something error",
+                    null
+                )
+            }
+
+        useCase.execute(
+            SearchBusinessesByKeywordUseCase.Params(
+                "Sample",
+                1,
+                1,
+                12.12,
+                123.123,
+                SortingStrategy.Default
+            )
+        )
+
+        Unit
     }
 
-    @Test
-    fun `should throw an exception when ioexception happened`() {
-        TODO("Not yet implemented")
+    @Test(expected = NoDataException::class)
+    fun `should throw an exception when no results for which relates to the keyword`() = runBlocking {
+        `when`(gateway.searchBusinesses(ArgumentMatchers.anyMap()))
+            .then {
+                throw NoDataException("No business is fetched")
+            }
+
+        useCase.execute(
+            SearchBusinessesByKeywordUseCase.Params(
+                "Sample",
+                1,
+                2,
+                12.213,
+                212.312,
+                SortingStrategy.Default
+            )
+        )
+
+        Unit
     }
 
-    @Test
-    fun `should throw an exception when limit param reached the maximum value 50`() {
-        TODO("Not yet implemented")
+    @Test(expected = IllegalArgumentException::class)
+    fun `should throw an exception when keyword is empty`() = runBlocking {
+
+        useCase.execute(
+            SearchBusinessesByKeywordUseCase.Params(
+                "",
+                1,
+                1,
+                12.12,
+                123.123,
+                SortingStrategy.Default
+            )
+        )
+
+        Unit
     }
+
 }
